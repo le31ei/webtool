dedf2f90244bf2150f6005f1ecf99a194fb2c1af
le31ei <le31ei@vip.qq.com>
2015-11-04 20:31:34 +0800

    v0.1

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint,render_template
+#coding=utf8
+from flask import Blueprint,render_template,redirect,url_for
 from app import app
 
 admins = Blueprint("admins", __name__, static_folder='static', template_folder='templates',url_prefix='/'+app.config['ADMIN_PATH'])
@@ -6,4 +7,16 @@ admins = Blueprint("admins", __name__, static_folder='static', template_folder='
 @admins.route('/')
 @admins.route('/login')
 def login():
+    """
+        登录函数 v0.1
+        升级：限制登录次数
+    :return:
+    """
+
     return render_template('admins/login.html')
+
+@admins.route('/logout')
+def logout():
+    return redirect(url_for("admins.login"))
+
+
