58bf193fdc0cd5b0e05a7090976363c02f7f9ef9
le31ei <le31ei@vip.qq.com>
2015-11-09 11:21:11 +0800

    modify config

---
+++ config.py
@@ -0,0 +1,16 @@
+#coding=utf8
+
+#调试
+DEBUG = True
+
+#后台路径
+ADMIN_PATH = 'xxx'
+
+
+#数据库
+SQLALCHEMY_DATABASE_URI = "mysql://xxx:xxx@localhost/webtool"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+#session 密钥
+SECRET_KEY = ''
